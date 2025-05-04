845b7b6b1ae5605f5a394963743e213e038df0d2
Dinasty29 <frandysrod@gmail.com>
2025-05-04 15:39:41 -0400

    Agregar todos los archivos al entorno

---
+++ app.js
@@ -0,0 +1,35 @@
+document.getElementById("mensaje").textContent="Hola Mundo";
+/* document.writeln("<h1>Hola Mundo</h1>");
+
+
+//array
+let arreglo1 = ['joe', 'ryan', 'martha'];
+let arreglo2 = [1,2,3];
+
+console.log(arreglo2);
+
+
+
+let password = "pepe123";
+let input = prompt("Ingrese su contrasena");
+
+let result = password == input;
+
+if(result==true){
+    console.log ("Acceso Autorizado");
+} else {
+    console.log ("No Autorizado");
+    }
+
+while(input!=password){
+    prompt("Ingrese su contrasena");    
+}
+  
+*/
+
+let count = 50;
+while(count > 0){
+    console.log(count);
+    count = count - 1;
+}
+  
